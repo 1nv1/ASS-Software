acaf4b0996f55c03dc950e03924f7c81a4ed937b
nelsci <nelson.lombardo@gmail.com>
2016-04-22 09:47:39 -0300

    Varias mejoras, ahora la señal X se desplaza de forma invertida para intentar ser más representativo en la animación. Si el parámetro de la duración de la animación (sexto) es cero, el proceso avanza haciendo click derecho sobre la gŕafica.

--- octave/conv_dis_ani.m
+++ octave/conv_dis_ani.m
@@ -2,11 +2,21 @@
 function conv_dis_ani(x, zx, h, zh, span, duration, delay)
   lx = length(x);
   lh = length(h);
+  if((zx<0)||(zx>lx))
+    disp("Error! Element zero of X is out of range.")
+    return
+  endif
+  if((zh<0)||(zh>lh))
+    disp("Error! Element zero of H is out of range.")
+    return
+  endif
   if(span < (lx+(2*lh)))
     disp("Error! span argument must be greater than length(x) + 2* length(h)")
     return
   endif
-  tani = round((duration*1e6)/span);
+  if(duration>0)
+    tani = round((duration*1e6)/span);
+  endif
   rang = round(-span/2):round(span/2);
   tempx = x;
   temph = h;
@@ -14,23 +24,31 @@ function conv_dis_ani(x, zx, h, zh, span, duration, delay)
   temph(numel(1:(lx+lh))) = 0;
   tempx(numel(1:(lx+lh))) = 0;
   temph = shift(temph, lx);
+  % Flip X
+  x = fliplr(x);
   x(numel(rang)) = 0;
   h(numel(rang)) = 0;
   swap = 0;
   sleep(delay);
-  initI = round(span/2)-zx-lh+1;
-  endI = round(span/2)+(lx-zh);
+  initI = round(span/2)-zh-lx+1;
+  endI = round(span/2)+(lh-zh);
   idTotal = 1;
   addSignals = zeros(1, length(rang));
   figure(1); clf;
-  for i = 1:span+1;
+  for i = 0:span+1;
+    figure(1);
     hold on;
     subplot(2,1,1);
     cla;
-    pa = stem(rang.+(round(span/2)-zx), x, "color", "r", "linewidth", 5);
-    pb = stem(rang.+i, h, "color", "b", "linewidth", 5);
+    pa = stem(rang.+(round(span/2)-zh), h, "color", "r", "linewidth", 5);
+    pb = stem(rang.+i, x, "color", "b", "linewidth", 5);
     axis([round(-span/2),round(span/2)]);
     hold off;
+    if(i==1)
+      subplot(2,1,2);
+      stem(rang, zeros(1, numel(rang)));
+      axis([round(-span/2),round(span/2),min([min(x),min(h)]),max([max(x),max(h)])]);
+    endif
     if((i>=initI)&&(i<=endI))
       temph = shift(temph,1);
       convtot = tempx .* temph;
@@ -39,11 +57,16 @@ function conv_dis_ani(x, zx, h, zh, span, duration, delay)
       hold on;
       subplot(2,1,2);
       cla;
-      stem(rang, shift(addSignals,initI+zh), "color", "g", "linewidth", 5);
-      axis([round(-span/2),round(span/2)]);
+      stem(rang, shift(addSignals,initI+zx), "color", "g", "linewidth", 5);
+      axis([round(-span/2),round(span/2),min(addSignals),max(addSignals)]);
       hold off;
     endif
-    usleep(tani);
+    if(duration>0)
+      usleep(tani);
+    else
+      figure(1);
+      waitforbuttonpress();
+    endif
   endfor
   hold off;
 endfunction
