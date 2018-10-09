1d90e7ee564ebba906a1d98df9d358d409abb52e
Nelson <nelson.lombardo@gmail.com>
2018-10-08 22:33:54 -0300

    Actualiazación
    Del script 2.4.8 para demostrar la mejora de la performance al usar el modelado
    recomendando para un software como Octave.

--- 07 - Series Complejas/2_4_8.m
+++ 07 - Series Complejas/2_4_8.m
@@ -1,16 +1,27 @@
-z = 0.0001 + 0.999i;
-loop = 4;
+z = 0.3223 + 0.112i;
+loop = 6;
 resA = 0;
 resA = log(1 + z);
 printf("       z: (%f)+j(%f)\n\n", real(z), imag(z))
 printf("Function: (%f)+j(%f)\n\n", real(resA), imag(resA))
 for m = 1:loop
   len = 10**m;
+  # Este sería el modelado tradicional usando una estructura FOR
+  time1 = time();
   resB = 0;
   for n = 0:len
     resB = (((-1)** n)*(z**(n + 1))/(n + 1)) + resB;
   end
-  printf("  Taylor: (%f)+j(%f) :: Ite: %f\n", real(resB), imag(resB), len)
+  time2 = time();
+  timeElapsed = (time2 - time1) * 1000;
+  printf("  Taylor: (%f)+j(%f) :: Ite: %d :: mSec: %f\n", real(resB), imag(resB), len, timeElapsed);
+  # La forma de modelar el anterior cálculo sería la siguiente
+  time1 = time();
+  n = [0:len];
+  resC = sum(((-1).**n).*(z.**(n + 1))./(n + 1));
+  time2 = time();
+  timeElapsed = (time2 - time1) * 1000;
+  printf("  Taylor: (%f)+j(%f) :: Ite: %d :: mSec: %f\n", real(resC), imag(resC), len, timeElapsed);
   printf("   Error: %f%%\n", abs(((abs(resA) / abs(resB))*100) - 100))
   printf("\n")
 end
