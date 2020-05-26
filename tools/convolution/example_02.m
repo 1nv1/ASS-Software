4e7f07a3fcfc4811d190eec9b4f9f5ddde51cdf3
nelsci <nelson.lombardo@gmail.com>
2020-05-26 00:16:56 -0300

    Added example 01-02 to convolution

---
+++ tools/convolution/example_02.m
@@ -0,0 +1,10 @@
+% Definimos el vector del tiempo 'continuo'
+dt = 0.05; % ~~50[ms]
+% El vector del tiempo coincide con un ciclo completo
+t = 0:dt:4;
+% La señal X
+X = ones(1, length(t));
+% La señal H
+H = t;
+% Asumimos que las señales comienzan en el instante 0 (cero)
+conv_ani(X, round(length(X)/2), H, round(length(H)/2), dt);
