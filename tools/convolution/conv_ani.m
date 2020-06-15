aaf3198a78e5bf37b4f971bd18eaf334e609dab0
nelsci <nelson.lombardo@gmail.com>
2020-06-15 00:24:31 -0300

    Function X drawed correctly

--- tools/convolution/conv_ani.m
+++ tools/convolution/conv_ani.m
@@ -1,19 +1,36 @@
-function conv_ani(fx, zx, fh, zh, dt = 0.5, duration = 5, delay = 0)
+function conv_ani(fx, zx, fh, zh, dt = 0.5, duration = 5, delay = 1)
+
+  function res = getIdx(f, i, dt, q)
+    for j=i:dt:q
+      if !(f(j) == 0) break; endif
+    endfor
+    res = j;
+  endfunction
+
+  function [rf, rz] = cleanSignal(f, z)
+    len = length(f);
+    i = getIdx(f, 1, 1, len);
+    q = getIdx(f, len, -1, 1);
+    rf = f(i:q);
+    rz = z - i;
+  endfunction
+
   %% graphics_toolkit("gnuplot");
   if (duration < 0)
     disp("Error! Duration must be greater than 0.")
     return
   endif
-  %% Get length of "functions"
-  len_x = length(fx);
-  len_h = length(fh);
 
-  if ((zx < 0) || (zx > len_x))
+  %% Get length of "functions"
+  x.len = length(fx);
+  h.len = length(fh);
+
+  if ((zx < 1) || (zx > x.len))
     disp("Error! Element zero of X is out of range.")
     return
   endif
-  if ((zh < 0) || (zh > len_h))
-    disp("Error! Element zero of H is out of range.")
+  if ((zh < 1) || (zh > h.len))
+    disp("Error! Element zero of Y is out of range.")
     return
   endif
 
@@ -21,104 +38,89 @@ function conv_ani(fx, zx, fh, zh, dt = 0.5, duration = 5, delay = 0)
   figure(1);
   clf;
 
-  %% Span calculation
-  span = (len_x + len_h) * 2;
+  %% Window calculation
+  win.len = (x.len + h.len) + 1;
+  %% Window half
+  win.half = round(win.len / 2);
+  win.cor = 0;
+  if (mod(win.len, 2))
+    win.cor = 1;
+    win.half = win.half - 1;
+  endif
+
   %% Original convolution
-  total_conv = conv(fx, fh) .* dt;
+  conv.original = conv(fx, fh) .* dt;
+
+  [x.f, x.z] = cleanSignal(fx, zx - 1);
+  [h.f, h.z] = cleanSignal(fh, zh - 1);
+  h.f = flip(h.f);
+
   %% Discrete time animation
-  tani = (duration/span);
-  %% Half span
-  hspan = round(span/2);
+  ani.dt = (duration/win.len);
+  ani.delay = delay;
+
   %% Final range
-  rang = -hspan:hspan;
-  len_r = length(rang);
-  %% Detect imperfection
-  cion = mod(len_r, 2);
-  if (cion) cion = 1; end
+  win.t.f = -win.half:win.half;
+  win.t.len = length(win.t);
 
-  %% Complete with zeros
-  init = hspan - zx + cion;
-  zi = zeros(1, init);
-  zq = zeros(1, span - init - len_x + cion);
-  fx = [zi fx zq];
-
-  % Flip H
-  fh = fliplr(fh);
-
-  if (dt == 1)
-    initI = hspan - zx - zh - (cion * 2);
-  else
-    initI = hspan - zx - zh - (round(cion / dt) * 2);
-  end
-  endI = hspan + len_x + len_h - zx - zh - 1;
-  initAni = hspan - len_x - zx + 1;
-  endAni = hspan + len_h - zh + zx + 1;
-  idTotal = 1;
-  addSignals = zeros(1, span);
-
-  %% Initial delay, 0 by default
-  pause(delay);
+  %% Initial delay, 1 sec. by default
+  pause(ani.delay);
 
   %% Get handlers
   subplot(2, 1, 1);
-  xa_data = rang;
-  ya_data = fx;
-  xb_data = rang;
-  yb_data = [fh zeros(1, span - len_h + 1)];
   hold on;
+  f = [zeros(1, win.half - x.z - win.cor) x.f zeros(1, win.len - (win.half + (x.len - x.z) - win.cor))];
   if (dt == 1)
-    pXH(1) = stem(xa_data, ya_data, 'linewidth', 4, 'color', 'red');
-    pXH(2) = stem(xb_data, yb_data, 'linewidth', 4, 'color', 'blue');
+    pSgs(1) = stem(win.t.f, f, 'linewidth', 4, 'color', 'red');
+    pSgs(2) = stem(nan, nan, 'linewidth', 4, 'color', 'blue');
   else
-    %pXH(1) = plot(xa_data, ya_data, 'linewidth', 4, 'color', 'red');
-    pXH(1) = area(xa_data, ya_data, "FaceColor", "red");
-    set(get(pXH(1), 'children'), 'facealpha', 0.5);
-    %pXH(2) = plot(xb_data, yb_data, 'linewidth', 4, 'color', 'blue');
-    pXH(2) = area(xb_data, yb_data, "FaceColor", "blue");
-    set(get(pXH(2), 'children'), 'facealpha', 0.5);
+    pSgs(1) = area(win.t.f, x.f, "FaceColor", "red");
+    set(get(pSgs(1), 'children'), 'facealpha', 0.5);
+    pSgs(2) = area(nan, nan, "FaceColor", "blue");
+    set(get(pSgs(2), 'children'), 'facealpha', 0.5);
   end
+  win.s.min = floor(min([min(fx),min(fh)]));
+  if (win.s.min > 0) win.s.min = 0; end
+  win.s.max = ceil(max([max(fx),max(fh)]));
+  if (win.s.max < 0) win.s.max = 0; end
+  win.s.axis = [-win.half, win.half, win.s.min, win.s.max];
+  axis(win.s.axis);
   grid on;
-  miny = floor(min([min(fx),min(fh)]));
-  if (miny > 0) miny = 0; end
-  maxy = ceil(max([max(fx),max(fh)]));
-  if (maxy < 0) maxy = 0; end
-  axisA = [-hspan, hspan, miny, maxy];
-  axis(axisA);
 
   subplot(2, 1, 2);
   hold on;
   if (dt == 1)
-    pY(1) = stem(nan, 'linewidth', 4, 'color', 'magenta');
+    pConv(1) = stem(nan, 'linewidth', 4, 'color', 'magenta');
   else
-    pY(1) = plot(nan, 'linewidth', 4, 'color', 'magenta');
+    pConv(1) = plot(nan, 'linewidth', 4, 'color', 'magenta');
   end
-  %% pY(2) = stem(nan, 'linewidth', 8, 'color', 'black');
+  win.c.min = floor(min(conv.original));
+  if (win.c.min > 0) win.c.min = 0; end
+  win.c.max = ceil(max(conv.original));
+  if (win.c.max < 0) win.c.max = 0; end
+  win.c.axis = [-win.half, win.half, win.c.min, win.c.max];
+  axis(win.c.axis);
   grid on;
-  miny = floor(min(total_conv));
-  if (miny > 0) miny = 0; end
-  maxy = ceil(max(total_conv));
-  if (maxy < 0) miny = 0; end
-  axisB = [-hspan, hspan, miny, maxy];
-  axis(axisB);
 
   %% y(t) -> 0
-  set(pY(1),'XData', rang, 'YData', zeros(1, length(rang)));
+  set(pConv(1),'XData', win.t.f, 'YData', zeros(1, win.len));
 
-  for i = 0:(span - len_h);
+  %% Graph until H touch the end of screen
+  for i = 1:(win.len - h.len);
     %% h(t)
-    xb_data = rang;
-    yb_data = [zeros(1, i) fh zeros(1, span - len_h - i + 1)];
-    set(pXH(2), 'XData', xb_data, 'YData', yb_data);
-    %% y(t)
-	  if ((i >= initAni) && (i <= endAni))
-	    convtot = dt .* fx .* yb_data;
-      addSignals(1, idTotal) = sum(convtot);
-	    idTotal++;
-      yt = shift(addSignals, initI + zx);
-      set(pY(1),'XData', rang, 'YData', yt)
-    end
-    %% Animation
-    drawnow();
-    pause(tani);
+    t = win.t.f;
+    f = [zeros(1, i) h.f zeros(1, win.len - h.len - i - 1)];
+    set(pSgs(2), 'XData', t, 'YData', f);
+  %%  %% y(t)
+	%%  if ((i >= initAni) && (i <= endAni))
+	%%    convtot = dt .* fx .* yb_data;
+  %%    addSignals(1, idTotal) = sum(convtot);
+	%%    idTotal++;
+  %%    yt = shift(addSignals, initI + zx);
+  %%    set(pY(1),'XData', rang, 'YData', yt)
+  %%  end
+  %%  %% Animation
+  %%  drawnow();
+  %%  pause(tani);
   end
 end
